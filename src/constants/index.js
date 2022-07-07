36278eb1c569e3b10abcf916cb961d9a27b4ce90
IndraSuandiardana <indrasuandiardana080820@gmail.com>
2022-07-07 19:59:22 +0800

    UAS Mobile

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+const APP_TOKEN = '9f43ad847689d7d457cfc2498c299d7650b2b1117282c95a760e69fad35baaee'
+const REQUEST_HEADER = {
+    Accept: 'application/json',
+    'Content-Type' : 'application/json',
+    Authorization: `Bearer ${APP_TOKEN}`
+}
+
+export { REQUEST_HEADER }
